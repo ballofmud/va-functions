aadb104a515434c3d43b182b297eb1115365367d
vxnano <vxnano@gazeta.pl>
2026-05-20 15:33:29 +0200

    return fixed ticker values

--- DniWolne.cs
+++ DniWolne.cs
@@ -7,6 +7,13 @@ namespace va.functions;
 
 public class DniWolne
 {
+    private class TickerDailyValue
+    {
+        public string Ticker { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Value { get; set; }
+    }
+
     private readonly ILogger<DniWolne> _logger;
 
     public DniWolne(ILogger<DniWolne> logger)
@@ -17,7 +24,13 @@ public class DniWolne
     [Function("DniWolne")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Ambarangada!");
+        var tickerDailyValues = new List<TickerDailyValue>
+        {
+            new TickerDailyValue { Ticker = "AAPL", Date = DateTime.UtcNow.Date, Value = 150.25m },
+            new TickerDailyValue { Ticker = "MSFT", Date = DateTime.UtcNow.Date, Value = 250.75m },
+            new TickerDailyValue { Ticker = "GOOGL", Date = DateTime.UtcNow.Date, Value = 2750.50m }
+        };
+        
+        return new OkObjectResult(tickerDailyValues);
     }
 }
